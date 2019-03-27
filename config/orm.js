57eaeb0b6b974fb3522b04024b0803bb1a90a3ac
Joshua Beverly <beverlyj93@hotmail.com>
2019-03-26 19:37:05 -0500

    Changes

--- config/orm.js
+++ config/orm.js
@@ -2,20 +2,20 @@ var connection = require('./connection');
 
 var orm = {
     selectAll: (table, cb) => {
-        connection.query('SELECT * FROM `burgers_db`.??;', [table], (err, result) => {
+        connection.query('SELECT * FROM `f40fw2jqiz1m1a0h`.??;', [table], (err, result) => {
             if(err) throw err;
             cb(result);
         })
     },
     insertOne: (table, col, val, cb) => {
-        connection.query('INSERT INTO `burgers_db`.?? (??) VALUES (?);', [table, col, val], (err, result) => {
+        connection.query('INSERT INTO `f40fw2jqiz1m1a0h`.?? (??) VALUES (?);', [table, col, val], (err, result) => {
             if(err) throw err;
             console.log('One was inserted.');
             cb(result);
         });
     },
     updateOne: (table, col, val, cond, condVal, cb) => {
-        connection.query('UPDATE `burgers_db`.?? SET ?? = ? WHERE ?? = ?', [table, col, val, cond, condVal], (err, result) => {
+        connection.query('UPDATE `f40fw2jqiz1m1a0h`.?? SET ?? = ? WHERE ?? = ?', [table, col, val, cond, condVal], (err, result) => {
             if(err) throw err;
             console.log('One was updated.');
             cb(result);
